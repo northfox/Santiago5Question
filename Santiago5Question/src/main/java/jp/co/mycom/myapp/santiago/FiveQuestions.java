d585abf5a283a654cc9041be8d217b718588d8a0
test <test@example.com>
2015-05-11 18:38:12 +0900

    Refactoring first by recursion

--- Santiago5Question/src/main/java/jp/co/mycom/myapp/santiago/FiveQuestions.java
+++ Santiago5Question/src/main/java/jp/co/mycom/myapp/santiago/FiveQuestions.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FiveQuestions {
-  
+
   public double firstSumByForLoop(List<Double> numbers) {
     // use bigdecimal because solve float error
     BigDecimal result = new BigDecimal(0.0);
@@ -33,25 +33,15 @@ public class FiveQuestions {
     // use bigdecimal because solve float error
     BigDecimal result = new BigDecimal(0.0);
     List<Double> removableNumbers = new ArrayList<Double>(numbers);
-    
-    result =
-        firstSumByRecursionBody(removableNumbers, removableNumbers.remove(0));
 
+    if (removableNumbers.size() != 0) {
+      result = new BigDecimal(removableNumbers.remove(0));
+      double addNumber = firstSumByRecursion(removableNumbers);
+      result = addBigDecimal(result, addNumber);
+    }
     return result.doubleValue();
   }
   
-  private BigDecimal firstSumByRecursionBody(List<Double> removableNumbers,
-      double number) {
-    BigDecimal result = new BigDecimal(number);
-
-    if (removableNumbers.size() != 0) {
-      BigDecimal addNumber =
-          firstSumByRecursionBody(removableNumbers, removableNumbers.remove(0));
-      result = addBigDecimal(result, addNumber.doubleValue());
-    }
-    return result;
-  }
-
   private BigDecimal addBigDecimal(BigDecimal result, double number) {
     BigDecimal addNumber = new BigDecimal(number);
     result = result.add(addNumber, new MathContext(4, RoundingMode.HALF_UP));

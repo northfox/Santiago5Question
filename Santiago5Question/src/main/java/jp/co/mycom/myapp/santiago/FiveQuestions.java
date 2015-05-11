9cf0ca6ec91c8d3522938d3fcafcc3877a365b3c
test <test@example.com>
2015-05-11 18:21:12 +0900

    Refactoring first

--- Santiago5Question/src/main/java/jp/co/mycom/myapp/santiago/FiveQuestions.java
+++ Santiago5Question/src/main/java/jp/co/mycom/myapp/santiago/FiveQuestions.java
@@ -7,28 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FiveQuestions {
-
+  
   public double firstSumByForLoop(List<Double> numbers) {
     // use bigdecimal because solve float error
-    MathContext mathContext = new MathContext(4, RoundingMode.HALF_UP);
     BigDecimal result = new BigDecimal(0.0);
 
     for (Double number : numbers) {
-      BigDecimal addNumber = new BigDecimal(number);
-      result = result.add(addNumber, mathContext);
+      result = addBigDecimal(result, number);
     }
     return result.doubleValue();
   }
 
   public double firstSumByWhileLoop(List<Double> numbers) {
     // use bigdecimal because solve float error
-    MathContext mathContext = new MathContext(4, RoundingMode.HALF_UP);
     BigDecimal result = new BigDecimal(0.0);
     List<Double> removableNumbers = new ArrayList<Double>(numbers);
 
     while (removableNumbers.size() != 0) {
-      BigDecimal addNumber = new BigDecimal(removableNumbers.remove(0));
-      result = result.add(addNumber, mathContext);
+      result = addBigDecimal(result, removableNumbers.remove(0));
     }
     return result.doubleValue();
   }
@@ -37,23 +33,28 @@ public class FiveQuestions {
     // use bigdecimal because solve float error
     BigDecimal result = new BigDecimal(0.0);
     List<Double> removableNumbers = new ArrayList<Double>(numbers);
+    
     result =
         firstSumByRecursionBody(removableNumbers, removableNumbers.remove(0));
 
     return result.doubleValue();
   }
-
+  
   private BigDecimal firstSumByRecursionBody(List<Double> removableNumbers,
       double number) {
-    MathContext mathContext = new MathContext(4, RoundingMode.HALF_UP);
-    BigDecimal addNumber = new BigDecimal(number);
+    BigDecimal result = new BigDecimal(number);
 
     if (removableNumbers.size() != 0) {
-      BigDecimal childAddNumber =
+      BigDecimal addNumber =
           firstSumByRecursionBody(removableNumbers, removableNumbers.remove(0));
-      addNumber = addNumber.add(childAddNumber, mathContext);
+      result = addBigDecimal(result, addNumber.doubleValue());
     }
-    return addNumber;
+    return result;
   }
 
+  private BigDecimal addBigDecimal(BigDecimal result, double number) {
+    BigDecimal addNumber = new BigDecimal(number);
+    result = result.add(addNumber, new MathContext(4, RoundingMode.HALF_UP));
+    return result;
+  }
 }
